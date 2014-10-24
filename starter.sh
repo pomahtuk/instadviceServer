ffbd9cc92619cf58daaad0949285aadcc8e44880
pomahtuk <pman89@ya.ru>
2014-10-24 23:34:38 +0200

    fix typo

--- starter.sh
+++ starter.sh
@@ -6,7 +6,7 @@ if [ $(ps aux | grep $USER | grep node | grep -v grep | wc -l | tr -s "\n") -eq 
 then
     export NODE_ENV=production
     export PATH=/usr/local/bin:$PATH
-    forever start --sourceDir /home/ubuntu/instadviceServer -o /home/ubuntu/instadviceServer/server.log server.js -l /home/ubuntu/instadviceServer/server.log -e /home/ubuntu/instadviceServer/server.log server.js >> /home/ubuntu/instadviceServer/server_log.txt 2>&1
-    forever start --sourceDir /home/ubuntu/instadviceServer -o /home/ubuntu/instadviceServer/worker.log worker.js -l /home/ubuntu/instadviceServer/server.log -e /home/ubuntu/instadviceServer/server.log worker.js >> /home/ubuntu/instadviceServer/worker_log.txt 2>&1
+    forever start --sourceDir /home/ubuntu/instadviceServer -o /home/ubuntu/instadviceServer/server.log -l /home/ubuntu/instadviceServer/server.log -e /home/ubuntu/instadviceServer/server.log server.js >> /home/ubuntu/instadviceServer/server_log.txt 2>&1
+    forever start --sourceDir /home/ubuntu/instadviceServer -o /home/ubuntu/instadviceServer/worker.log -l /home/ubuntu/instadviceServer/server.log -e /home/ubuntu/instadviceServer/server.log worker.js >> /home/ubuntu/instadviceServer/worker_log.txt 2>&1
 fi
 
