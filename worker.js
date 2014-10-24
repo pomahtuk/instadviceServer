fb66d9dd8886e589ac123cd2a33a37a93b00528e
pomahtuk <pman89@ya.ru>
2014-10-24 23:21:02 +0200

    wip

--- worker.js
+++ worker.js
@@ -77,14 +77,14 @@ if (cluster.isMaster) {
 
   });
 
-  jobs.on('job complete', function(id,result){
-    kue.Job.get(id, function(err, job){
-      if (err) return;
-      job.remove(function(err){
-        if (err) throw err;
-        console.log('removed completed job #%d', job.id);
-      });
-    });
-  });
+  // jobs.on('job complete', function(id,result){
+  //   kue.Job.get(id, function(err, job){
+  //     if (err) return;
+  //     job.remove(function(err){
+  //       if (err) throw err;
+  //       console.log('removed completed job #%d', job.id);
+  //     });
+  //   });
+  // });
 
 }
